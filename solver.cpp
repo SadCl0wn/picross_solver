cea3862670a707b533e848427c87eed5d0c72626
morillon fançois <francois.morillon@yahoo.fr>
2018-09-12 15:28:22 +0200

    petit commit

--- solver.cpp
+++ solver.cpp
@@ -1,16 +1,60 @@
 #include "./solver.h"
 
-bool rightMove_x(char **matrix,char x){
-    
+bool rightMove_x(vector<vector<char>> &matrix, char x, vector<vector<char>> &rule_x)
+{
+    vector<char> nbOnes;
+    char n = 0;
+    char lastValue = 255;
+    for (auto i : matrix[x])
+    {
+        if (i == 1)
+        {
+            nbOnes[n]++;
+            if (rule_x[x][n] < nbOnes[n])
+                return false;
+        }
+        else if (lastValue == 1)
+        {
+            n++;
+            nbOnes.push_back(0);
+            if (rule_x[x].size() < nbOnes.size())
+                return false;
+        }
+        lastValue = i;
+    }
+    return true;
 }
-bool rightMove_y(char **matrix,char y){
-
+bool rightMove_y(vector<vector<char>> &matrix, char y, vector<vector<char>> &rule_y)
+{
+    vector<char> nbOnes;
+    char n = 0;
+    char lastValue = 255;
+    for (char x;x < matrix.size();x++)
+    {
+        char i = matrix[x][y];
+        if (i == 1)
+        {
+            nbOnes[n]++;
+            if (rule_y[y][n] < nbOnes[n])
+                return false;
+        }
+        else if (lastValue == 1)
+        {
+            n++;
+            nbOnes.push_back(0);
+            if (rule_y[y].size() < nbOnes.size())
+                return false;
+        }
+        lastValue = i;
+    }
+    return true;
 }
-bool rightMove(char **matrix,char x,char y){
-    rightMove_x(matrix, x);
-    rightMove_y(matrix, y);
+bool rightMove(vector<vector<char>> &matrix, char x, char y, vector<vector<char>> &rule_x, vector<vector<char>> &rule_y)
+{
+    rightMove_x(matrix, x, rule_x);
+    rightMove_y(matrix, y, rule_y);
 }
 
-bool solver(char **matrix){
-
+bool solver(vector<vector<char>> &matrix, vector<vector<char>> &rule_x, vector<vector<char>> &rule_y)
+{
 }
--- solver.h
+++ solver.h
@@ -1,2 +1,9 @@
 #pragma once
+#include <vector>
 
+using namespace std;
+
+bool rightMove_x(vector<vector<char>>& matrix,char x,vector<vector<char>> &rule_x);
+bool rightMove_y(vector<vector<char>>& matrix,char y,vector<vector<char>> &rule_y);
+bool rightMove(vector<vector<char>>& matrix,char x,char y,vector<vector<char>> &rule_x,vector<vector<char>> &rule_y);
+bool solver(vector<vector<char>>& matrix,vector<vector<char>> &rule_x,vector<vector<char>> &rule_y);
